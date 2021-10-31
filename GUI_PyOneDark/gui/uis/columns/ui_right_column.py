2ee2940d134d33fa943b203e490ef14b182dba56
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-01 02:56:28 +0700

    Add Print status toggle

--- GUI_PyOneDark/gui/uis/columns/ui_right_column.py
+++ GUI_PyOneDark/gui/uis/columns/ui_right_column.py
@@ -1,23 +1,14 @@
-# ///////////////////////////////////////////////////////////////
-#
-# BY: WANDERSON M.PIMENTA
-# PROJECT MADE WITH: Qt Designer and PySide6
-# V: 1.0.0
-#
-# This project can be used freely for all uses, as long as they maintain the
-# respective credits only in the Python scripts, any information in the visual
-# interface (GUI) can be modified without any implication.
-#
-# There are limitations on Qt licenses if you want to use your products
-# commercially, I recommend reading them on the official website:
-# https://doc.qt.io/qtforpython/licenses.html
-#
-# ///////////////////////////////////////////////////////////////
+# -*- coding: utf-8 -*-
 
-# IMPORT QT CORE
-# ///////////////////////////////////////////////////////////////
+################################################################################
+## Form generated from reading UI file 'right_columnDEuHeP.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
 from qt_core import *
-
 
 class Ui_RightColumn(object):
     def setupUi(self, RightColumn):
@@ -36,16 +27,55 @@ class Ui_RightColumn(object):
         self.verticalLayout.setSpacing(5)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.btn_4_widget = QWidget(self.menu_1)
+        self.btn_4_widget.setObjectName(u"btn_4_widget")
+        self.btn_4_widget.setMinimumSize(QSize(0, 40))
+        self.btn_4_widget.setMaximumSize(QSize(16777215, 40))
+        self.btn_4_layout = QVBoxLayout(self.btn_4_widget)
+        self.btn_4_layout.setSpacing(0)
+        self.btn_4_layout.setObjectName(u"btn_4_layout")
+        self.btn_4_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.verticalLayout.addWidget(self.btn_4_widget)
+
         self.btn_1_widget = QWidget(self.menu_1)
         self.btn_1_widget.setObjectName(u"btn_1_widget")
         self.btn_1_widget.setMinimumSize(QSize(0, 40))
         self.btn_1_widget.setMaximumSize(QSize(16777215, 40))
-        self.btn_1_layout = QVBoxLayout(self.btn_1_widget)
-        self.btn_1_layout.setSpacing(0)
-        self.btn_1_layout.setObjectName(u"btn_1_layout")
-        self.btn_1_layout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout = QHBoxLayout(self.btn_1_widget)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.Nightmode = QLabel(self.btn_1_widget)
+        self.Nightmode.setObjectName(u"Nightmode")
+
+        self.horizontalLayout.addWidget(self.Nightmode)
+
+        self.ToggleNight = QFrame(self.btn_1_widget)
+        self.ToggleNight.setObjectName(u"ToggleNight")
+        self.ToggleNight.setMaximumSize(QSize(40, 40))
+        self.ToggleNight.setFrameShape(QFrame.StyledPanel)
+        self.ToggleNight.setFrameShadow(QFrame.Raised)
+        self.ToggleNightmode = QVBoxLayout(self.ToggleNight)
+        self.ToggleNightmode.setSpacing(0)
+        self.ToggleNightmode.setObjectName(u"ToggleNightmode")
+        self.ToggleNightmode.setContentsMargins(0, 0, 0, 0)
+
+        self.horizontalLayout.addWidget(self.ToggleNight)
+
 
         self.verticalLayout.addWidget(self.btn_1_widget)
+
+        self.btn_3_widget = QWidget(self.menu_1)
+        self.btn_3_widget.setObjectName(u"btn_3_widget")
+        self.btn_3_widget.setMinimumSize(QSize(0, 40))
+        self.btn_3_widget.setMaximumSize(QSize(16777215, 40))
+        self.btn_3_layout = QVBoxLayout(self.btn_3_widget)
+        self.btn_3_layout.setSpacing(0)
+        self.btn_3_layout.setObjectName(u"btn_3_layout")
+        self.btn_3_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.verticalLayout.addWidget(self.btn_3_widget)
 
         self.label_1 = QLabel(self.menu_1)
         self.label_1.setObjectName(u"label_1")
@@ -58,50 +88,13 @@ class Ui_RightColumn(object):
         self.verticalLayout.addWidget(self.label_1)
 
         self.menus.addWidget(self.menu_1)
-        self.menu_2 = QWidget()
-        self.menu_2.setObjectName(u"menu_2")
-        self.verticalLayout_2 = QVBoxLayout(self.menu_2)
-        self.verticalLayout_2.setSpacing(5)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.verticalLayout_2.setContentsMargins(5, 5, 5, 5)
-        self.btn_2_widget = QWidget(self.menu_2)
-        self.btn_2_widget.setObjectName(u"btn_2_widget")
-        self.btn_2_widget.setMinimumSize(QSize(0, 40))
-        self.btn_2_widget.setMaximumSize(QSize(16777215, 40))
-        self.btn_2_layout = QVBoxLayout(self.btn_2_widget)
-        self.btn_2_layout.setSpacing(0)
-        self.btn_2_layout.setObjectName(u"btn_2_layout")
-        self.btn_2_layout.setContentsMargins(0, 0, 0, 0)
-
-        self.verticalLayout_2.addWidget(self.btn_2_widget)
-
-        self.label_2 = QLabel(self.menu_2)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setFont(font)
-        self.label_2.setStyleSheet(u"font-size: 16pt")
-        self.label_2.setAlignment(Qt.AlignCenter)
-
-        self.verticalLayout_2.addWidget(self.label_2)
-
-        self.label_3 = QLabel(self.menu_2)
-        self.label_3.setObjectName(u"label_3")
-        font1 = QFont()
-        font1.setPointSize(9)
-        self.label_3.setFont(font1)
-        self.label_3.setStyleSheet(u"font-size: 9pt")
-        self.label_3.setAlignment(Qt.AlignCenter)
-        self.label_3.setWordWrap(True)
-
-        self.verticalLayout_2.addWidget(self.label_3)
-
-        self.menus.addWidget(self.menu_2)
 
         self.main_pages_layout.addWidget(self.menus)
 
 
         self.retranslateUi(RightColumn)
 
-        self.menus.setCurrentIndex(1)
+        self.menus.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(RightColumn)
@@ -109,9 +102,7 @@ class Ui_RightColumn(object):
 
     def retranslateUi(self, RightColumn):
         RightColumn.setWindowTitle(QCoreApplication.translate("RightColumn", u"Form", None))
+        self.Nightmode.setText(QCoreApplication.translate("RightColumn", u"Toggle Nightmode", None))
         self.label_1.setText(QCoreApplication.translate("RightColumn", u"Menu 1 - Right Menu", None))
-        self.label_2.setText(QCoreApplication.translate("RightColumn", u"Menu 2 - Right Menu", None))
-        self.label_3.setText(QCoreApplication.translate("RightColumn", u"This is just an example menu.\n"
-"Add Qt Widgets or your custom widgets here.", None))
     # retranslateUi
 
