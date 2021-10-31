dc53b5cc1248c1266e5543ba6f522637a9438da1
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-01 03:44:44 +0700

    New right_column

--- GUI_PyOneDark/gui/uis/columns/ui_right_column.py
+++ GUI_PyOneDark/gui/uis/columns/ui_right_column.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'right_columnDEuHeP.ui'
+## Form generated from reading UI file 'right_columnqTRRqG.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.0
 ##
@@ -14,7 +14,7 @@ class Ui_RightColumn(object):
     def setupUi(self, RightColumn):
         if not RightColumn.objectName():
             RightColumn.setObjectName(u"RightColumn")
-        RightColumn.resize(240, 600)
+        RightColumn.resize(240, 544)
         self.main_pages_layout = QVBoxLayout(RightColumn)
         self.main_pages_layout.setSpacing(0)
         self.main_pages_layout.setObjectName(u"main_pages_layout")
@@ -27,33 +27,38 @@ class Ui_RightColumn(object):
         self.verticalLayout.setSpacing(5)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(5, 5, 5, 5)
-        self.btn_4_widget = QWidget(self.menu_1)
-        self.btn_4_widget.setObjectName(u"btn_4_widget")
-        self.btn_4_widget.setMinimumSize(QSize(0, 40))
-        self.btn_4_widget.setMaximumSize(QSize(16777215, 40))
-        self.btn_4_layout = QVBoxLayout(self.btn_4_widget)
-        self.btn_4_layout.setSpacing(0)
-        self.btn_4_layout.setObjectName(u"btn_4_layout")
-        self.btn_4_layout.setContentsMargins(0, 0, 0, 0)
-
-        self.verticalLayout.addWidget(self.btn_4_widget)
-
-        self.btn_1_widget = QWidget(self.menu_1)
-        self.btn_1_widget.setObjectName(u"btn_1_widget")
-        self.btn_1_widget.setMinimumSize(QSize(0, 40))
-        self.btn_1_widget.setMaximumSize(QSize(16777215, 40))
-        self.horizontalLayout = QHBoxLayout(self.btn_1_widget)
+        self.row_1 = QWidget(self.menu_1)
+        self.row_1.setObjectName(u"row_1")
+        self.row_1.setMinimumSize(QSize(0, 40))
+        self.row_1.setMaximumSize(QSize(16777215, 40))
+        self.row_1.setStyleSheet(u"border-top-style: solid;\n"
+"border-bottom-style: solid;\n"
+"border-width: 1px;\n"
+"border-color: #2c313c;")
+        self.horizontalLayout = QHBoxLayout(self.row_1)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.Nightmode = QLabel(self.btn_1_widget)
+        self.ToggleNight_Icon = QFrame(self.row_1)
+        self.ToggleNight_Icon.setObjectName(u"ToggleNight_Icon")
+        self.ToggleNight_Icon.setMaximumSize(QSize(30, 40))
+        self.ToggleNight_Icon.setFrameShape(QFrame.StyledPanel)
+        self.ToggleNight_Icon.setFrameShadow(QFrame.Raised)
+        self.ToggleNightmode_3 = QVBoxLayout(self.ToggleNight_Icon)
+        self.ToggleNightmode_3.setSpacing(0)
+        self.ToggleNightmode_3.setObjectName(u"ToggleNightmode_3")
+        self.ToggleNightmode_3.setContentsMargins(0, 0, 0, 0)
+
+        self.horizontalLayout.addWidget(self.ToggleNight_Icon)
+
+        self.Nightmode = QLabel(self.row_1)
         self.Nightmode.setObjectName(u"Nightmode")
 
         self.horizontalLayout.addWidget(self.Nightmode)
 
-        self.ToggleNight = QFrame(self.btn_1_widget)
+        self.ToggleNight = QFrame(self.row_1)
         self.ToggleNight.setObjectName(u"ToggleNight")
-        self.ToggleNight.setMaximumSize(QSize(40, 40))
+        self.ToggleNight.setMaximumSize(QSize(60, 40))
         self.ToggleNight.setFrameShape(QFrame.StyledPanel)
         self.ToggleNight.setFrameShadow(QFrame.Raised)
         self.ToggleNightmode = QVBoxLayout(self.ToggleNight)
@@ -64,18 +69,22 @@ class Ui_RightColumn(object):
         self.horizontalLayout.addWidget(self.ToggleNight)
 
 
-        self.verticalLayout.addWidget(self.btn_1_widget)
+        self.verticalLayout.addWidget(self.row_1)
 
-        self.btn_3_widget = QWidget(self.menu_1)
-        self.btn_3_widget.setObjectName(u"btn_3_widget")
-        self.btn_3_widget.setMinimumSize(QSize(0, 40))
-        self.btn_3_widget.setMaximumSize(QSize(16777215, 40))
-        self.btn_3_layout = QVBoxLayout(self.btn_3_widget)
+        self.row_2 = QWidget(self.menu_1)
+        self.row_2.setObjectName(u"row_2")
+        self.row_2.setMinimumSize(QSize(0, 40))
+        self.row_2.setMaximumSize(QSize(16777215, 40))
+        self.row_2.setStyleSheet(u"border-top-style: solid;\n"
+"border-bottom-style: solid;\n"
+"border-width: 1px;\n"
+"border-color: #2c313c;")
+        self.btn_3_layout = QVBoxLayout(self.row_2)
         self.btn_3_layout.setSpacing(0)
         self.btn_3_layout.setObjectName(u"btn_3_layout")
         self.btn_3_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.verticalLayout.addWidget(self.btn_3_widget)
+        self.verticalLayout.addWidget(self.row_2)
 
         self.label_1 = QLabel(self.menu_1)
         self.label_1.setObjectName(u"label_1")
