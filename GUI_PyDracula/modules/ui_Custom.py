d8b1bfc43e0f4d338a0b3b55cd7b65b48c4bc70f
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-02 23:20:10 +0700

    Add setup_gui

--- GUI_PyDracula/modules/ui_Custom.py
+++ GUI_PyDracula/modules/ui_Custom.py
@@ -1,11 +1,40 @@
+from main import *
 from widgets.py_toggle import PyToggle
 
 class ui_Custom:
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
     def addWidget(self):
+        widgets = self.ui
+
         Toggle_NightMode = PyToggle()
-        self.ui.Toggle_Night_Layout.addWidget(Toggle_NightMode)
+        widgets.Toggle_Night_Layout.addWidget(Toggle_NightMode)
+
         Toggle_Close = PyToggle()
-        self.ui.Toggle_Close_Layout.addWidget(Toggle_Close)
+        widgets.Toggle_Close_Layout.addWidget(Toggle_Close)
+
         Toggle_Sound = PyToggle()
-        self.ui.Toggle_Sound_Layout.addWidget(Toggle_Sound)
+        widgets.Toggle_Sound_Layout.addWidget(Toggle_Sound)
+
+    def setup_gui(self):
+        widgets = self.ui
+
+        # USE CUSTOM TITLE BAR | USE AS "False" FOR MAC OR LINUX
+        Settings.ENABLE_CUSTOM_TITLE_BAR = True
+        # APP NAME
+        title = "Right Posture"
+        description = "Right Posture - Make life better."
+
+        # APPLY TEXTS
+        self.setWindowTitle(title)
+        widgets.titleRightInfo.setText(description)
+
+        # QTableWidget PARAMETERS
+        widgets.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # QTableWidget PARAMETERS
+        widgets.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
