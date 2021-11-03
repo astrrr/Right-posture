b7cb9c7f3524976d753671f8c4c01f1f116c768c
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-04 02:36:52 +0700

    Update addWidget

--- GUI_PyDracula/modules/ui_Custom.py
+++ GUI_PyDracula/modules/ui_Custom.py
@@ -2,24 +2,19 @@ from main import *
 from widgets.py_toggle import *
 
 class ui_Custom:
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-    def addWidget(self):
-        widgets = self.ui
-
-        Toggle_NightMode = PyToggle_1()
-
-        widgets.Toggle_Night_Layout.addWidget(Toggle_NightMode)
-
-        Toggle_Close = PyToggle_2()
-        widgets.Toggle_Close_Layout.addWidget(Toggle_Close)
-
-        Toggle_Sound = PyToggle_3()
-
-        widgets.Toggle_Sound_Layout.addWidget(Toggle_Sound)
+    # def addWidget(self):
+    #     widgets = self.ui
+    #
+    #     Toggle_NightMode = PyToggle_1()
+    #
+    #     widgets.Toggle_Night_Layout.addWidget(Toggle_NightMode)
+    #
+    #     Toggle_Close = PyToggle_2()
+    #     widgets.Toggle_Close_Layout.addWidget(Toggle_Close)
+    #
+    #     Toggle_Sound = PyToggle_3()
+    #
+    #     widgets.Toggle_Sound_Layout.addWidget(Toggle_Sound)
 
     def setup_gui(self):
         widgets = self.ui
