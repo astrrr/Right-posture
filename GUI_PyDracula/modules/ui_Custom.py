1cca5f2d8d7edbb75344dbc01068949627789515
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-10 02:39:58 +0700

    Refactor ui_Custom.py

--- GUI_PyDracula/modules/ui_Custom.py
+++ GUI_PyDracula/modules/ui_Custom.py
@@ -1,17 +1,7 @@
 from main import *
 
-
-
-
 class ui_Custom:
-
-    def load(self):
-        with open("bin/save_setting.json", "r") as read_file:
-            loaded_object = json.load(read_file)
-        return loaded_object
 
     def setup_gui(self):
         widgets = self.ui
-
-
-
+        pass
