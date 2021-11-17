6cdd9112d0ca8aa4d5e2ae4022043facaa309ce6
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-18 04:53:56 +0700

    Fix bottomMenu color when hover&pressed

--- GUI_PyDracula/modules/app_settings.py
+++ GUI_PyDracula/modules/app_settings.py
@@ -1,4 +1,4 @@
-class Settings:
+class Settings():
     # APP SETTINGS
     # ///////////////////////////////////////////////////////////////
     ENABLE_CUSTOM_TITLE_BAR = True
@@ -8,7 +8,7 @@ class Settings:
     TIME_ANIMATION = 500
 
     # BTNS LEFT AND RIGHT BOX COLORS
-    BTN_LEFT_BOX_COLOR = "background-color: rgb(44, 49, 58);"
+    BTN_LEFT_BOX_COLOR = "background-color: rgb(44, 49, 58); border-color:rgb(44, 49, 58);"
     BTN_RIGHT_BOX_COLOR = "background-color: #ff79c6;"
 
     # MENU SELECTED STYLESHEET
