ba5edaac89e4b39727e0db1fe8622e6878ec1384
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2021-11-27 21:28:20 +0700

    fix bug in login page

--- GUI_PyDracula/modules/ui_login.py
+++ GUI_PyDracula/modules/ui_login.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'loginJdKXKZ.ui'
+## Form generated from reading UI file 'loginYeBAgb.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.1
 ##
