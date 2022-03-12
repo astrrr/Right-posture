8021fa685d595248ef2c63aea428a1226dfb37dd
Sekson Thongon <invisibile.sek@gmail.com>
2022-03-12 20:18:09 +0700

    Fix correct/incorrect time

--- Right Posture/modules/app_detect.py
+++ Right Posture/modules/app_detect.py
@@ -28,6 +28,8 @@ t_incorrect_last = 0
 t_incorrect_total = 0
 t_total = 0
 tick_flag = 0
+cor_count = 0
+inc_count = 0
 
 start_time = time.asctime(time.localtime(time.time()))
 
@@ -95,7 +97,7 @@ class VideoThread(QThread):
 
     # QThreadのrunメソッドを定義
     def run(self):
-        global t_incorrect_last, t_start, t_incorrect_total, t_total, tick_flag
+        global t_incorrect_last, t_start, t_incorrect_total, t_total, tick_flag, cor_count, inc_count
         
         if Camera_detail.First_load_model:
             print("Start Load model")
@@ -132,15 +134,17 @@ class VideoThread(QThread):
                     
                     if pred == 0:
                         cv2.putText(image, "Correct", (20, 20), 2, 0.5, (0, 255, 0), 1)
-                        if tick_flag==1 :
-                            t_incorrect_total = t_incorrect_total + (time.time() - t_incorrect_last)
-                        tick_flag = 0
+                        cor_count+=1
+                        # if tick_flag==1 :
+                        #     t_incorrect_total = t_incorrect_total + (time.time() - t_incorrect_last)
+                        # tick_flag = 0
                         
                     if pred == 1:
                         cv2.putText(image, "Incorrect", (20, 20), 2, 0.5, (0, 0, 255), 1)
-                        if tick_flag == 0:
-                            t_incorrect_last = time.time()
-                        tick_flag = 1
+                        inc_count+=1
+                        # if tick_flag == 0:
+                        #     t_incorrect_last = time.time()
+                        # tick_flag = 1
                         
                             
 
@@ -169,16 +173,17 @@ class VideoThread(QThread):
                         self.change_pixmap_signal.emit(image)
                 
                 end_time = time.asctime(time.localtime(time.time()))
-                t_incorrect_total = t_incorrect_total + (time.time() - t_incorrect_last)
+                
 
                 
                 f = open('temp.txt','r')
                 user = f.read()
                 print(f.read())
                 t_total = time.time() - t_start
-                t_cor = t_total - t_incorrect_total
-                inc_per = (t_incorrect_total/t_total)*100
-                cor_per = ((t_total-t_incorrect_total)/t_total)*100
+                t_incorrect_total = (inc_count/(inc_count+cor_count))*t_total
+                t_cor = (cor_count/(inc_count+cor_count))*t_total
+                inc_per = (inc_count/(inc_count+cor_count))*100
+                cor_per = (cor_count/(inc_count+cor_count))*100
                 print('t_incorrect_last  : ',t_incorrect_last)
                 print('t_incorrect       : {} sec'.format(t_incorrect_total))
                 print('t_cor             : {} sec'.format(t_cor))
