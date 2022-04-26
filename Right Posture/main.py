099422bb46de264048bf440ddab83253af4b4f61
Kridsanapong Buathongjun <69898533+ussnllmn@users.noreply.github.com>
2022-04-26 15:35:33 +0700

    Update version to 1.2.1.0 !!!

--- Right Posture/main.py
+++ Right Posture/main.py
@@ -32,7 +32,7 @@ os.environ["QT_FONT_DPI"] = "96" # FIX Problem for High DPI and Scale above 100%
 
 # main "1" = MainWindow , main "0" = AuthWindow
 main = 0
-version.thisVersion = "1.2.0.0"
+version.thisVersion = "1.2.1.0"
 # /////////////////////////////////////////////
 counter = 0
 CircularProgress_timer = 300
